24f0820bd764778fc7ab070e20dedf2eb002b707
Lizardo-Castillo <132401266+Lizardo-Castillo@users.noreply.github.com>
2023-06-27 18:00:54 -0500

    Delete farmacia-prueba directory

--- farmacia-prueba/recibDeleteLab.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('config.php');
-$idRegistros = $_REQUEST['id'];
-$DeleteRegistro = ("DELETE FROM laboratorios WHERE id_laboratorio= '".$idRegistros."' ");
-mysqli_query($con, $DeleteRegistro);
-?>
--- farmacia-prueba/recib_Delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('config.php');
-$idRegistros = $_REQUEST['id'];
-
-$DeleteRegistro = ("DELETE FROM clientes WHERE id= '".$idRegistros."' ");
-mysqli_query($con, $DeleteRegistro);
-?>
